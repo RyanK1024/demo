a44ea5af4a9de7ceaf53bb989ab9f778cf472020
ryan <ryan@1024.com>
2023-04-20 19:41:56 +0800

    hot-fix commit1

--- src/main/java/com/controllers/DemoControl.java
+++ src/main/java/com/controllers/DemoControl.java
@@ -3,5 +3,6 @@ package com.controllers;
 public class DemoControl {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
